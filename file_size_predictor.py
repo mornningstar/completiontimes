771dedf41a8d7fde8a782a716a034791b3cb4525
Nastasja Parschew <nastasja.parschew@informatik.hu-berlin.de>
2024-04-02 15:00:16 +0200

    added tslearn class for file size prediction

---
+++ file_size_predictor.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from tslearn.svm import TimeSeriesSVR
+from tslearn.utils import to_time_series_dataset
+
+
+class FileSizePredictor:
+
+    def __init__(self, file_history):
+        self.file_history = file_history
+        self.model = TimeSeriesSVR(kernel="gak")
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+
+        self.ts_dataset = None
+        self.sizes_normalised = None
+
+    def prepare_data(self):
+        sizes = np.array([record['size'] for record in self.file_history]).reshape(-1, 1)
+        self.sizes_normalized = self.scaler.fit_transform(sizes)
+
+        self.ts_dataset = to_time_series_dataset(sizes_normalized)
+
+    def train_model(self):
+        if self.ts_dataset is not None and self.sizes_normalized is not None:
+            X_train = self.ts_dataset[:-1]
+            y_train = self.sizes_normalized[1:].ravel() #Predicting the next size
+            self.model.fit(X_train, y_train)
+        else:
+            raise ValueError("Data has not been prepared. Call prepare_data() before training.")
+
+    def predict_next_size(self):
+        if self.ts_dataset is not None:
+            last_point = self.ts_dataset[-1].reshape(1, -1, 1)
+            next_size_normalised = self.model.predict(last_point)
+            next_size = self.scaler.inverse_transform(next_size_normalised.reshape(-1, 1))
+            return next_size[0][0]
+        else:
+            raise ValueError("Model has not been trained. Call train_model() after prepare_data().")
+
